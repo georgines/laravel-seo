86324d1fe0a3a8ec09dcf7c97ea0ac3b72e6f193
Bruno Gaspar <brunofgaspar1@gmail.com>
2015-07-09 22:34:24 +0100

    Added copyright headers.
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- src/Traits/SeoableTrait.php
+++ src/Traits/SeoableTrait.php
@@ -1,5 +1,23 @@
 <?php
 
+/**
+ * Part of the Laravel Seo package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Laravel Seo
+ * @version    1.0.0
+ * @author     Werxe LTD
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2015, Werxe LTD
+ * @link       http://werxe.com
+ */
+
 namespace Werxe\Seo\Traits;
 
 trait SeoableTrait
